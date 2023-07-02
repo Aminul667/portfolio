feca3fa9478a32377c12e94bd1fdebcee80defe9
Aminul667 <rahathssn@gmail.com>
2023-07-02 21:28:08 +0600

    added banner

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,48 @@
+import { FaDownload, FaGithub, FaLinkedin } from "react-icons/fa/index.esm";
+import "./Home.css";
+import { BiSolidContact } from "react-icons/bi/index.esm";
+import myResume from "../../assets/Resume_Md_Aminul_Islam.pdf";
+
+const Home = () => {
+  return (
+    <div className="font-antique banner-container text-white">
+      {/* <h2 className="font-medium text-3xl">Hello</h2> */}
+      <h2 className="text-7xl font-medium text-center">
+        Hello, I am Md. Aminul Islam
+      </h2>
+      <div className="flex justify-center items-center gap-5 mt-8">
+        <a
+          href="https://github.com/Aminul667"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="text-5xl hover:text-orange-400"></FaGithub>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/md-aminul"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="text-5xl hover:text-orange-400"></FaLinkedin>
+        </a>
+      </div>
+      <div className="flex gap-4 justify-center items-center mt-8">
+        <a
+          href={myResume}
+          download="Resume_Md_Aminul_Islam"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="font-medium text-3xl flex gap-3 items-center px-5 py-3 border-2 rounded-md hover:border-orange-400">
+            Resume <FaDownload></FaDownload>
+          </button>
+        </a>
+        <button className="font-medium text-3xl flex gap-3 items-center px-5 py-3 border-2 rounded-md hover:border-orange-400">
+          Contact <BiSolidContact></BiSolidContact>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
