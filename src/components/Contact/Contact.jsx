a05f1b4e5066193785b04f5151de46d9eb85b667
Aminul667 <rahathssn@gmail.com>
2023-07-03 19:55:33 +0600

    added contact information

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,78 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { BiLogoGmail } from "react-icons/bi/index.esm";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="px-28 py-6 mt-10 bg-neutral-900 text-white font-antique">
+      <h2 className="text-5xl font-normal mb-4">Get in Touch</h2>
+      <div className="flex justify-between gap-7">
+        <div className="w-1/2">
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="mb-3">
+              {/* <label className="block">Name</label> */}
+              <input
+                type="text"
+                name="from_name"
+                placeholder="Name"
+                className="rounded-md bg-neutral-800 w-full px-3 py-2 focus:outline-none focus:border-orange-500 focus:ring-1 focus:ring-orange-400"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="email"
+                name="from_email"
+                placeholder="Email"
+                className="rounded-md bg-neutral-800 w-full px-3 py-2 focus:outline-none focus:border-orange-500 focus:ring-1 focus:ring-orange-400"
+              />
+            </div>
+            <div className="mb-3">
+              {/* <label>Message</label> */}
+              <textarea
+                name="message"
+                placeholder="Message"
+                rows="5"
+                className="rounded-md bg-neutral-800 w-full px-3 py-2 focus:outline-none focus:border-orange-500 focus:ring-1 focus:ring-orange-400"
+              />
+            </div>
+            <input
+              type="submit"
+              value="Send"
+              className="w-full font-medium text-xl px-5 py-1 rounded-md bg-orange-500 hover:bg-orange-600"
+            />
+          </form>
+        </div>
+        <div>
+          <p className="text-2xl mb-5">Please feel free to reach me at the contact information below</p>
+          <div>
+          <a href="mailto:aminulislamrahat134@gmail.com" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 text-xl hover:text-orange-400 mb-3"><BiLogoGmail></BiLogoGmail> aminulislamrahat134@gmail.com</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
       <About></About>
+      <Contact></Contact>
     </>
   );
 };
