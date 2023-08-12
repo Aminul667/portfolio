d9328cd927063c326f2b8db18227b68ecb88106c
Aminul667 <rahathssn@gmail.com>
2023-08-12 22:40:21 +0600

    added blog link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,60 @@
 import { HashLink } from "react-router-hash-link";
 
 const NavBar = () => {
+  const scrollWithOffset = (el, offset) => {
+    const elementPosition = el.offsetTop - offset;
+    window.scroll({
+      top: elementPosition,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <nav className="text-center text-xl">
       <div className="text-white font-antique w-full p-4 bg-neutral-900 opacity-80 fixed top-0 z-10">
-        <HashLink to="#home" smooth className="mr-10">Home</HashLink>
-        <HashLink to="#about" smooth className="mr-10">About</HashLink>
-        <HashLink to="#skills" smooth className="mr-10">Skills</HashLink>
-        <HashLink to="#projects" smooth className="mr-10">Projects</HashLink>
-        <HashLink to="#contact" smooth className="mr-10">Contact</HashLink>
+        <HashLink to="#home" smooth className="mr-10">
+          Home
+        </HashLink>
+        <HashLink
+          to="#about"
+          smooth
+          className="mr-10"
+          scroll={(el) => scrollWithOffset(el, 100)}
+        >
+          About
+        </HashLink>
+        <HashLink
+          to="#skills"
+          smooth
+          className="mr-10"
+          scroll={(el) => scrollWithOffset(el, 80)}
+        >
+          Skills
+        </HashLink>
+        <HashLink
+          to="#projects"
+          smooth
+          className="mr-10"
+          scroll={(el) => scrollWithOffset(el, 80)}
+        >
+          Projects
+        </HashLink>
+        <HashLink
+          to="#contact"
+          smooth
+          className="mr-10"
+          scroll={(el) => scrollWithOffset(el, 80)}
+        >
+          Contact
+        </HashLink>
+        <a
+          href="https://eclectic-c5aa0.web.app/category/all"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Blog
+        </a>
       </div>
     </nav>
   );
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,7 +7,13 @@
 // import nodejs from "../../assets/images/node.jpg";
 // import ex from "../../assets/images/express.png";
 // import mongo from "../../assets/images/mongodb.png";
-import { FaBootstrap, FaCss3Alt, FaHtml5, FaNodeJs, FaReact } from "react-icons/fa/index.esm";
+import {
+  FaBootstrap,
+  FaCss3Alt,
+  FaHtml5,
+  FaNodeJs,
+  FaReact,
+} from "react-icons/fa/index.esm";
 import { SiExpress, SiMongodb } from "react-icons/si/index.esm";
 import { TbBrandJavascript, TbBrandTailwind } from "react-icons/tb/index.esm";
 
@@ -15,13 +21,16 @@ const Skills = () => {
   //   const itemList = [html, css, js, tw, bs, reactjs, nodejs, ex, mongo];
 
   return (
-    <div className="px-8 lg:px-24 mt-10 font-antique text-3xl lg:text-5xl" id="skills">
+    <div
+      className="px-8 lg:px-24 mt-10 font-antique text-3xl lg:text-5xl"
+      id="skills"
+    >
       <h2 className="font-normal mb-4">Skills</h2>
-      <div className="flex flex-col lg:flex-row">
+      {/* <div className="flex flex-col lg:flex-row">
         <div className="lg:px-28 lg:w-1/2">
           <h3 className="text-xl font-bold mb-5">Frontend development</h3>
           <div className="flex items-center gap-5 mb-3">
-            <FaHtml5></FaHtml5>
+            <FaHtml5 className="text-orange-700 text-[100px] border border-red-700"></FaHtml5>
             <progress
               className="progress progress-accent w-full h-6"
               value="80"
@@ -95,6 +104,23 @@ const Skills = () => {
               max="100"
             ></progress>
           </div>
+        </div>
+      </div> */}
+      <div className="grid grid-cols-4 gap-4 items-center justify-center">
+        <div className="text-center border border-red-600 m-auto">
+          <FaHtml5 className="text-orange-700 text-[150px]"></FaHtml5>
+        </div>
+        <div className="text-center border border-red-600 m-auto">
+          <FaCss3Alt className="text-orange-700 text-[150px]"></FaCss3Alt>
+        </div>
+        <div className="text-center border border-red-600 m-auto">
+          <TbBrandJavascript className="text-orange-700 text-[150px]"></TbBrandJavascript>
+        </div>
+        <div className="text-center border border-red-600 m-auto">
+          <TbBrandTailwind className="text-orange-700 text-[150px]"></TbBrandTailwind>
+        </div>
+        <div className="text-center border border-red-600 m-auto">
+          <FaBootstrap className="text-orange-700 text-[150px]"></FaBootstrap>
         </div>
       </div>
     </div>
