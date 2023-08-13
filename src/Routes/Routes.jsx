b8eee201cf0938721070d684b60f1177b8108bba
Aminul667 <rahathssn@gmail.com>
2023-08-13 18:07:42 +0600

    added base route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,17 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../components/Home/Home";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
     {
       path: "/",
       element: <Main></Main>,
       children: [
         {
-            path: '/',
-            element: <Home></Home>
-        }
-      ]
+          path: "/",
+          element: <Home></Home>,
+        },
+      ],
     },
-  ]);
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/portfolio/" }
+);
 
-  export default router;
+export default router;
