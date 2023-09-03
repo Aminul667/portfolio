2003038c1ef3a8b905cf0aae5b0b4379bb4bd7db
Aminul667 <rahathssn@gmail.com>
2023-09-03 23:40:46 +0600

    added changes to skils components

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -16,36 +16,36 @@ const Skills = () => {
       id="skills"
     >
       <h2 className="font-normal mb-4">Skills</h2>
-      <div className="grid lg:grid-cols-4 gap-4">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-4">
         <div className="text-center border border-orange-700 p-3 rounded-lg m-auto">
-          <FaHtml5 className="text-orange-700 text-[150px]"></FaHtml5>
+          <FaHtml5 className="text-orange-700 text-[100px] md:text-[150px]"></FaHtml5>
         </div>
         <div className="text-center border border-blue-700 p-3 rounded-lg m-auto">
-          <FaCss3Alt className="text-blue-700 text-[150px]"></FaCss3Alt>
+          <FaCss3Alt className="text-blue-700 text-[100px] md:text-[150px]"></FaCss3Alt>
         </div>
         <div className="text-center border border-yellow-500 p-3 rounded-lg m-auto">
-          <TbBrandJavascript className="text-yellow-500 text-[150px]"></TbBrandJavascript>
+          <TbBrandJavascript className="text-yellow-500 text-[100px] md:text-[150px]"></TbBrandJavascript>
         </div>
         <div className="text-center border border-blue-500 p-3 rounded-lg m-auto">
-          <TbBrandTailwind className="text-blue-500 text-[150px]"></TbBrandTailwind>
+          <TbBrandTailwind className="text-blue-500 text-[100px] md:text-[150px]"></TbBrandTailwind>
         </div>
         <div className="text-center border border-purple-600 p-3 rounded-lg m-auto">
-          <FaBootstrap className="text-purple-600 text-[150px]"></FaBootstrap>
+          <FaBootstrap className="text-purple-600 text-[100px] md:text-[150px]"></FaBootstrap>
         </div>
         <div className="text-center border border-cyan-500 p-3 rounded-lg m-auto">
-          <FaReact className="text-cyan-500 text-[150px]"></FaReact>
+          <FaReact className="text-cyan-500 text-[100px] md:text-[150px]"></FaReact>
         </div>
         <div className="text-center border border-green-500 p-3 rounded-lg m-auto">
-          <FaNodeJs className="text-green-500 text-[150px]"></FaNodeJs>
+          <FaNodeJs className="text-green-500 text-[100px] md:text-[150px]"></FaNodeJs>
         </div>
         <div className="text-center border border-gray-600 p-3 rounded-lg m-auto">
-          <SiExpress className="text-gray-600 text-[150px]"></SiExpress>
+          <SiExpress className="text-gray-600 text-[100px] md:text-[150px]"></SiExpress>
         </div>
         <div className="text-center border border-green-800 p-3 rounded-lg m-auto">
-          <SiMongodb className="text-green-800 text-[150px]"></SiMongodb>
+          <SiMongodb className="text-green-800 text-[100px] md:text-[150px]"></SiMongodb>
         </div>
         <div className="text-center border border-blue-800 p-3 rounded-lg m-auto">
-          <FaPython className="text-blue-800 text-[150px]"></FaPython>
+          <FaPython className="text-blue-800 text-[100px] md:text-[150px]"></FaPython>
         </div>
       </div>
     </div>
