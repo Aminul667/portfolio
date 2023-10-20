9520d48b62457f910f1a4c29705e3eddeb89350b
Aminul667 <rahathssn@gmail.com>
2023-10-21 02:03:47 +0600

    added new about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,31 +12,20 @@ const About = () => {
         className="w-[150px] h-[150px] rounded-full lg:w-[300px] lg:h-auto lg:rounded-l-[50%] lg:rounded-r-[50%]"
       />
       <div>
-        <h2 className="text-3xl text-center lg:text-left lg:text-5xl font-normal mb-4">About Me</h2>
+        <h2 className="text-3xl text-center lg:text-left lg:text-5xl font-normal mb-4">
+          About Me
+        </h2>
         <p className="text-lg mb-3">
-          I am Aminul, a skilled and versatile front-end developer proficient in
-          HTML, CSS, JavaScript, Tailwind, Bootstrap, React, MongoDB, and
-          ExpressJS. With a passion for crafting visually stunning and intuitive
-          user interfaces, I bring a strong attention to detail and a keen eye
-          for design to every project I undertake.
+          Hello, I am Aminul, and I am a mathematics enthusiast with a profound love for programming. Graduating with a degree in mathematics has equipped me with a solid foundation in analytical thinking and problem-solving. But my journey extends beyond the world of equations and proofs, as I have discovered a deep passion for the art of coding and software development.
         </p>
         <p className="text-lg mb-3">
-          Collaboration is at the core of my work ethic, and I thrive in team
-          environments where I can effectively communicate and bring ideas to
-          life. I have a proven track record of successfully partnering with
-          designers, product managers, and stakeholders to deliver exceptional
-          results on time and within budget.
+        For me, programming is not just a skill. It is a creative outlet and a means to bring abstract mathematical concepts to life. I revel in the process of crafting lines of code to build practical and innovative solutions. Whether it is developing web applications, optimizing algorithms, or delving into the intricacies of data analysis, I find programming to be a source of inspiration and empowerment.
         </p>
         <p className="text-lg mb-3">
-          As a natural problem solver, I relish challenges and approach them
-          with a proactive and innovative mindset. I am adaptable and thrive in
-          fast-paced environments, where I can leverage my strong analytical
-          skills to identify and implement efficient solutions.
+          As a natural problem solver, I relish challenges and approach them with a proactive and innovative mindset. I am adaptable and thrive in fast-paced environments, where I can leverage my strong analytical skills to identify and implement efficient solutions.
         </p>
         <p className="text-lg mb-3">
-          Continuous learning is an integral part of my professional journey. I
-          am committed to staying up-to-date with the latest industry trends and
-          technologies, always seeking new opportunities to enhance my skill set
+          Continuous learning is an integral part of my professional journey. I am committed to staying up-to-date with the latest industry trends and technologies, always seeking new opportunities to enhance my skill set
           and deliver cutting-edge solutions.
         </p>
       </div>
