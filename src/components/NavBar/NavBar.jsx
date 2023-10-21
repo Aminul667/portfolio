6b961d1c82e6624cd61c7ae8be40ecf7c1ad23ff
Aminul667 <rahathssn@gmail.com>
2023-10-21 17:33:03 +0600

    added responsive nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,8 +11,8 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="text-center text-lg lg:text-xl">
-      <div className="text-white font-playfair w-full p-4 bg-neutral-900 opacity-80 fixed top-0 z-10">
+    <nav className="text-center text-lg lg:text-xl font-playfair">
+      <div className="text-white w-full p-4 bg-neutral-900 opacity-80 fixed top-0 z-10 hidden lg:block">
         <HashLink
           to="#home"
           smooth
@@ -68,6 +68,38 @@ const NavBar = () => {
         >
           Blog
         </a>
+      </div>
+      <div className="dropdown lg:hidden bg-neutral-900 opacity-80 fixed top-0 z-10">
+        <label tabIndex={0} className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
+          </svg>
+        </label>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>Homepage</a>
+          </li>
+          <li>
+            <a>Portfolio</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
