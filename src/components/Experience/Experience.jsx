a576a0ed4f36ed30a24aaa6366f7b94c209595ae
Aminul667 <rahathssn@gmail.com>
2023-12-30 23:08:58 +0600

    added teaching experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -25,8 +25,40 @@ const Experience = () => {
       id: 4,
       companyPosition:
         "Python Instructor (Part-Time), Sofwebs Academy, Bandar Sunway, Malaysia",
-      skills: "Python, NumPy, Pandas, Matplotlib, Seaborn, Data Manipulation, Data Visualization",
+      skills:
+        "Python, NumPy, Pandas, Matplotlib, Seaborn, Data Manipulation, Data Visualization",
       date: "Dec 2019 – Jan 2021",
+    },
+  ];
+
+  const teachingExperience = [
+    {
+      id: 1,
+      companyPosition:
+        "Python Instructor (Part-Time), Happy Coders, Dhaka, Bangladesh",
+      skills: "Python Data Type and Data Structure, Conditional Statements, Loops, Functions, lambda expression, zip, filter, map",
+      date: "Dec 2022 – Apr 2023",
+    },
+    {
+      id: 2,
+      companyPosition:
+        "Mentor of C program, Euclid Programming Club, Department of Mathematics, SUST",
+      skills: "Introduction to C, Data Type, Conditional Statements, Function, Loops, Data Structure and Algorithm",
+      date: "Mar 2015 – Aug 2019",
+    },
+    {
+      id: 3,
+      companyPosition:
+        "Mentor of Mathematics (Part-Time), UCC academic and admission care (Private), Sylhet, Bangladesh",
+      skills: "teaching a curriculum equivalent to 'A level",
+      date: "Apr 2015 – Nov 2019",
+    },
+    {
+      id: 4,
+      companyPosition:
+        "Mentor of ICT (Part-Time), Ambition Coaching Home (Private), Sylhet, Bangladesh",
+      skills: "I worked as a part-time ICT (Information and Communication Technology) teacher",
+      date: "Oct 2015 – Aug 2019",
     },
   ];
 
@@ -46,6 +78,24 @@ const Experience = () => {
             </p>
             <p className="text-lg font-semibold">
               Skills: {professional.skills}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="ml-5">
+        <h3 className="text-2xl mb-4 font-medium">Teaching Experience</h3>
+        {teachingExperience.map((teaching) => (
+          <div
+            key={teaching.id}
+            className="border-l-2 border-orange-400 px-4 py-1 mb-5"
+          >
+            <p className="text-2xl font-semibold">{teaching.date}</p>
+            <p className="text-lg font-semibold">
+              {teaching.companyPosition}
+            </p>
+            <p className="text-lg font-semibold">
+              Skills: {teaching.skills}
             </p>
           </div>
         ))}
